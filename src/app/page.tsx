6e0bcc43854d9a51f6c52e4606aecd2a0b4d1fe9
hysa99 <eneahysa4@gmail.com>
2024-04-07 13:39:50 +0200

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,92 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24">
+
+      <header>
+        <div className="container flex justify-center items-center">
+          <img className="rounded-full h-24 w-24 md:h-32 md:w-32" src="https://media.licdn.com/dms/image/D4D03AQG4STMtYTSY9w/profile-displayphoto-shrink_800_800/0/1695494328964?e=1717632000&v=beta&t=XYvDvTfygY1t2d6SjJ40nwsCPmgzkcs3rZkEUjUZ6nw" alt="profile picture"/>
+          <h1 className="text-2xl md:text-4xl font-bold ml-4">Enea Hysa</h1>
         </div>
-      </div>
+        <div className="flex justify-center mt-4">
+          <p className="flex items-center"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/32/new-post.png" alt="new-post"/> eneahysa4@gmail.com</p>
+          <span className="mx-2">|</span>
+          <p className="flex items-center"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/flat-round/32/phone.png" alt="phone"/> (+355)/(0) 693857457</p>
+          <span className="mx-2">|</span>
+          <p className="flex items-center"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/32/place-marker--v1.png" alt="place-marker--v1"/> Tirane, Albania</p>
+          <span className="mx-2">|</span>
+          <p className="flex items-center"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/48/linkedin.png" alt="linkedin"/> Enea Hysa Linkedin Profile</p>
+          <span className="mx-2">|</span>
+          <p className="flex items-center"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/windows/32/github.png" alt="github"/> Enea Hysa Github Profile</p>
+        </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <hr className="my-8"/>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        
+        <div className="bg-gray-800 rounded-lg p-4 text-white">
+          <h1 className="text-2xl font-bold mb-4">My Skills</h1>
+          <ul>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/32/python--v1.png" alt="python--v1"/>Python</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/ios-filled/32/django.png" alt="django"/> Django</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/plasticine/32/react.png" alt="react"/> React js</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/32/settings-3--v1.png" alt="settings-3--v1"/> Automation Tester / QA (Python + Selenium)</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/nolan/32/web-design.png" alt="web-design"/> Web Design / Development</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/32/figma--v1.png" alt="figma--v1"/> Figma</li>
+          </ul>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="bg-gray-800 rounded-lg p-4  text-white">
+          <h1 className="text-2xl font-bold mb-4">My Experiences</h1>
+          <div>
+            <h2>Eder Studio</h2>
+            <p>Web Designer (Internship) - From Nov 2020 to Jan 2021</p>
+            <p>Learning About React js</p>
+            <p>Creating from scratch the frontend of a Websites</p>
+          </div>
+          <div>
+            <h2>Freelance | Self Employed (Working on personal projects)</h2>
+            <p>Web Development - From Nov 2019 to Present</p>
+            <p>Create Websites for different purposes with all the important features such as CRUD functions</p>
+            <p>React js | Django | HTML+CSS | Nextjs</p>
+            <p>Learning New Things | Improving Skills</p>
+          </div>
+          
+          <div>
+            <h2>MP (Revelop)</h2>
+            <p>Automation Tester | QA Automation Tester - From Oct 2021 to Present</p>
+            <p>First Line Support | Integrations - From Dec 2022 to Present</p>
+            <p>Create Scripts for Automation</p>
+            <p>Root Causes Analysis</p>
+            <p>Implementing / Modifying New Solutions</p>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <div className="bg-gray-800 rounded-lg p-4  text-white">
+          <h1 className="text-2xl font-bold mb-4">My Interests</h1>
+          <ul>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/32/python--v1.png" alt="python--v1"/>Python</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/ios-filled/32/django.png" alt="django"/> Django</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/plasticine/32/react.png" alt="react"/> React js</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/32/settings-3--v1.png" alt="settings-3--v1"/> Automation Tester / QA (Python + Selenium)</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/nolan/32/web-design.png" alt="web-design"/> Web Design / Development</li>
+            <li className="flex items-center mb-2"><img className="h-6 w-6 mr-2" src="https://img.icons8.com/color/32/figma--v1.png" alt="figma--v1"/> Figma</li>
+          </ul>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+
+      <hr className="my-8"/>
+
+      <section>
+        <div>
+          <h1 className="text-2xl">My Projects</h1>
+          <code className="bg-red-700 text-white">
+            404 Error Page / Coming Soon
+          </code>
+        </div>
+      </section>
+
     </main>
   );
 }
